25e8fd5e67a319cb22527700b9d0c4dc8c764f85
Luke Ku <lukeku@Lukes-MacBook-Pro.local>
2024-01-28 23:41:12 -0800

    Updated testac.cpp

--- testac.cpp
+++ testac.cpp
@@ -26,17 +26,51 @@ void testAC02() {
   Autocomplete ac;
   ac.readFile("cities.txt");
   auto v = ac.complete("Sea");
+
   assert(v.size() == 47);
   assert(v[0].first == "Seattle, Washington, United States");
   assert(v[0].second == 608660);
   assert(v[46].first == "Seabeck, Washington, United States");
   assert(v[46].second == 1105);
-  cout << "Ending tesAC02" << endl;
+  cout << "Ending testAC02" << endl;
+}
+
+void testAC03() {
+  cout << "Starting AC test03" << endl;
+  cout << "* Testing cities autocomplete" << endl;
+  Autocomplete ac;
+  ac.readFile("cities.txt");
+  auto v = ac.complete("Tai");
+
+  assert(v.size() == 34);
+  assert(v[0].first == "Taipei, Taiwan");
+  assert(v[0].second == 7871900);
+  assert(v[29].first == "Taipa, New Zealand");
+  assert(v[29].second == 1731);
+  cout << "Ending testAC03" << endl;
+}
+
+void testAC04() {
+  cout << "Starting AC test04" << endl;
+  cout << "* Testing cities autocomplete" << endl;
+  Autocomplete ac;
+  ac.readFile("cities.txt");
+  auto v = ac.complete("Los");
+
+  assert(v.size() == 106);
+  assert(v[0].first == "Los Angeles, California, United States");
+  assert(v[0].second == 3792621);
+  assert(v[14].first == "Los Altos, California, United States");
+  assert(v[14].second == 28976);
+  assert(v[98].first == "Los Conquistadores, Argentina");
+  assert(v[98].second == 1214);
+  cout << "Ending testAC04" << endl;
 }
 
 // // Calling all test functions
 void testACAll() {
   testAC01();
   testAC02();
-  // TODO(student) Add more tests
+  testAC03();
+  testAC04();
 }
